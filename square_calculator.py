c38748451ab2f79cf3efa3c1b62bffd763cbd0ad
Paul2004-sza <sutzawaungnhkum@gmail.com>
2025-01-23 22:12:53 +0700

    Added error handling for invalid inputs

--- square_calculator.py
+++ square_calculator.py
@@ -2,5 +2,8 @@ def calculate_square(number):
     return number ** 2
 
 if __name__ == "__main__":
-    num = int(input("Enter a number: "))
-    print(f"The square of {num} is {calculate_square(num)}")
+    try:
+        num = int(input("Enter a number: "))
+        print(f"The square of {num} is {calculate_square(num)}")
+    except ValueError:
+        print("Please enter a valid number!")
